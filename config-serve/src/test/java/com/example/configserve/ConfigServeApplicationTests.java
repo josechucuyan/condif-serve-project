a9f6557b53b1d4788e619a093750ade5bbde6629
unknown <josedachu@gmail.com>
2022-02-07 13:25:51 +0100

    Proyecyo con la carpeta de config

---
+++ config-serve/src/test/java/com/example/configserve/ConfigServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
